a1ddad57adf750471351b524f8b1f18d65c190de
Pedro Shin <pedromaschio.shin@gmail.com>
2024-08-06 11:47:36 -0300

    mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
 
+config();
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(String(process.env.MONGODB_URI)),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
