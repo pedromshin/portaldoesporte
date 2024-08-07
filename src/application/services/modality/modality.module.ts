52edbfbb1cb9c296a9d6273c20653f828c8e807d
Pedro Shin <pedromaschio.shin@gmail.com>
2024-08-07 19:12:02 -0300

    relative schema

--- src/application/services/modality/modality.module.ts
+++ src/application/services/modality/modality.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ModalityService } from './modality.service';
 import { ModalityController } from './modality.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ModalitySchema } from '@schemas/modality.schema';
+import { ModalitySchema } from '../../../schemas/modality.schema';
 import { Modality } from '../../../entities/modality.entity';
 
 @Module({
